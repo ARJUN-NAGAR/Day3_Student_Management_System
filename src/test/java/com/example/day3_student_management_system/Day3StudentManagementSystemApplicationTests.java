3a86d747a93c851151a23f8dbac5f143c221eee1
ajnr2456 <arjunnagar486@gmail.com>
2026-01-20 11:49:01 +0530

    First Commit in Day3_Student_Management_Student

---
+++ src/test/java/com/example/day3_student_management_system/Day3StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3StudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
